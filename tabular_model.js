64faff757ed7e48a3e001d8c93f8681c4365a725
Михаил Редреев <105384308+mRedreev@users.noreply.github.com>
2025-10-21 00:50:21 +0300

    Update tabular_model.js

--- tabular_model.js
+++ tabular_model.js
@@ -12,11 +12,11 @@ export function buildTabularModel(schema) {
   for (const h of schema.catCols) {
     const size = schema.catMaps[h].size;
     const dim = Math.min(50, Math.ceil(Math.sqrt(size))+1);
-    const inp = tf.input({shape: [], dtype:'int32', name: `cat_${h}`});
-    inputs.push(inp);
-    const emb = tf.layers.embedding({inputDim: size, outputDim: dim}).apply(inp);
-    const flat = tf.layers.flatten().apply(emb);
-    parts.push(flat);
+    const inp = tf.input({shape: [1], dtype:'int32', name: `cat_${h}`});
+inputs.push(inp);
+const emb = tf.layers.embedding({inputDim: size, outputDim: dim}).apply(inp);
+const flat = tf.layers.flatten().apply(emb); 
+parts.push(flat);
   }
 
   const concat = tf.layers.concatenate().apply(parts);
